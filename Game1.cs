7c13b942840988c2ec03b925ffbcc646129f84e7
Matteo Severini <severini.matt@gmail.com>
2022-01-19 08:40:11 +0000

    Nettoyage Game1.cs

--- Game1.cs
+++ Game1.cs
@@ -13,22 +13,11 @@ namespace lost_clothes_code
 {
     public class Game1 : Game
     {
-        private const int HAUTEUR_PERSO = 45;
         private const int HAUTEUR_FENETRE = 450;
         private const int LARGEUR_FENETRE = 765;
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-        private Vector2 _persoPosition;
-        private AnimatedSprite _perso;
-        private int _vitessePerso;
-        private int _vitesseMarche;     // nombre de pas par seconde pour l'_animationPerso
-        private Stopwatch _stopWatchMarche;
-        private Stopwatch _stopWatchSaut;
-        private Stopwatch _stopWatchChute;
-        private string _animationPerso;
-        private SpriteFont _font;
-        private Vector2 _positionTexte;
         private readonly ScreenManager _screenManager;
         private bool _menuStarted;
 
@@ -48,30 +37,14 @@ namespace lost_clothes_code
 
             // TODO: Add your initialization logic here
 
-            _menuStarted = false;
-            _persoPosition.X = 100;
-            _persoPosition.Y = 400;
-            _vitessePerso = 200;
-            _vitesseMarche = 2;
-            _stopWatchMarche = new Stopwatch();
-            _stopWatchMarche.Start();
-            _stopWatchSaut = new Stopwatch();
-            _stopWatchChute = new Stopwatch();
-            _animationPerso = "d_idle";
-            _positionTexte = new Vector2(0, 0);
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-
-            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("chevalier_0.sf", new JsonContentLoader());
-            _perso = new AnimatedSprite(spriteSheet);
-            _font = Content.Load<SpriteFont>("font");
         }
 
         protected override void Update(GameTime gameTime)
@@ -79,107 +52,7 @@ namespace lost_clothes_code
             _graphics.PreferredBackBufferHeight = HAUTEUR_FENETRE;
             _graphics.PreferredBackBufferWidth = LARGEUR_FENETRE;
             _graphics.ApplyChanges();
-            /*
 
-
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
-
-            float deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            float walkSpeed = deltaSeconds * _vitessePerso;
-            string sensHorizontal = "D";    // G = gauche, D = droite
-            string sensVertical = "N";      // N = neutre, H = haut, B = bas
-
-            KeyboardState keyboardState = Keyboard.GetState();
-
-            if (keyboardState.IsKeyDown(Keys.Up))
-            {
-                _stopWatchSaut.Start();
-                _stopWatchChute.Start();
-                sensVertical = "H";
-            }
-
-            if (keyboardState.IsKeyDown(Keys.Left))
-            {
-                if (_stopWatchMarche.ElapsedMilliseconds >= 1000.0 / _vitesseMarche || _animationPerso.Substring(0, 1) == "d")
-                {
-                    if (sensVertical == "H")
-                    {
-                        if (_animationPerso == "g_jumping_2")
-                            _animationPerso = "g_jumping_1";
-                        else
-                            _animationPerso = "g_jumping_2";
-                    }
-                    else
-                    {
-                        if (_animationPerso == "g_walking_2")
-                            _animationPerso = "g_walking_1";
-                        else
-                            _animationPerso = "g_walking_2";
-                    }
-                    _stopWatchMarche.Restart();
-                }
-                _persoPosition.X -= walkSpeed;
-            }
-            else if (keyboardState.IsKeyDown(Keys.Right))
-            {
-                if (_stopWatchMarche.ElapsedMilliseconds >= 1000.0 / _vitesseMarche || _animationPerso.Substring(0, 1) == "g")
-                {
-                    if (sensVertical == "H")
-                    {
-                        if (_animationPerso == "d_jumping_2")
-                            _animationPerso = "d_jumping_1";
-                        else
-                            _animationPerso = "d_jumping_2";
-                    }
-                    else
-                    {
-                        if (_animationPerso == "d_walking_2")
-                            _animationPerso = "d_walking_1";
-                        else
-                            _animationPerso = "d_walking_2";
-                    }
-                    _stopWatchMarche.Restart();
-                }
-                _persoPosition.X += walkSpeed;
-            }
-
-            if (_stopWatchSaut.IsRunning)
-                _persoPosition.Y -= walkSpeed;
-
-            if (_persoPosition.Y < GraphicsDevice.Viewport.Height - HAUTEUR_PERSO / 2)
-                _persoPosition.Y += _stopWatchChute.ElapsedMilliseconds * walkSpeed / 600;
-            else
-            {
-                _persoPosition.Y = GraphicsDevice.Viewport.Height - HAUTEUR_PERSO / 2;
-                _stopWatchSaut.Reset();
-                _stopWatchChute.Reset();
-            }
-
-            keyboardState = Keyboard.GetState();
-            if (keyboardState.IsKeyDown(Keys.Space))
-            {
-                LoadScreen1_0();
-            }
-            if (keyboardState.IsKeyDown(Keys.A))
-            {
-                LoadScreen1_1();
-            }
-            if(keyboardState.IsKeyDown(Keys.Z))
-            {
-                LoadScreenMenu();
-            }
-            if (keyboardState.IsKeyDown(Keys.P))
-            {
-                LoadScreen4_1();
-            }
-
-
-            _perso.Play(_animationPerso);
-            _perso.Update(deltaSeconds);
-            */
             if (!_menuStarted)
             {
                 _menuStarted = true;
